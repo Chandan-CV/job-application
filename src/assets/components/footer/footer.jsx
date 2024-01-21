4a5a4fdf23b27cb48783b4e43af15f99e7c0f8cb
Thorn <subhrajyoti.ghosh@outlook.com>
2024-01-22 01:24:52 +0530

    changed router, cover, footer

--- src/assets/components/footer/footer.jsx
+++ src/assets/components/footer/footer.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
 import styles from './footer.module.css'
 
 const Footer = () => {
   return (
     <div className={styles.footer}>
       <div className={styles.left}>
-        <span >
+        <span className={styles.contact}>
         <img src='phone.svg'  className={styles.icon}/>
         <span>+91 87963759562</span>
         </span>
-        <span >
+        <span className={styles.contact}>
         <img src='contact.svg' className={styles.icon}/>
         <span>xyz@gmail.com</span>
         </span>
--- src/assets/pages/home/home.jsx
+++ src/assets/pages/home/home.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import styles from './home.module.css'
+import styles from "./home.module.css";
+
 const Home = () => {
   return (
-    <div className={styles.home}>
+  <div className={styles.home}>
+    <div className="background-image">
+      <img className="cover-image" src="/home.png" />
     </div>
-  )
-}
+  </div>
+  );
+};
 
-export default Home
+export default Home;
