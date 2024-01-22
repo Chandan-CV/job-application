2cf7d590f99e074b92d4a4a1f71ad13d5b8fa020
Thorn <subhrajyoti.ghosh@outlook.com>
2024-01-22 10:33:47 +0530

    cover img

--- src/assets/pages/home/home.jsx
+++ src/assets/pages/home/home.jsx
@@ -3,8 +3,8 @@ import styles from "./home.module.css";
 const Home = () => {
   return (
   <div className={styles.home}>
-    <div className="background-image">
-      <img className="cover-image" src="/home.png" />
+    <div className={styles.background}>
+      <img className={styles.cover} src="/home.png" />
     </div>
   </div>
   );
