e9264be27e5226f998f72c53568af3504cf67932
dragoon4890 <harshsks123@gmail.com>
2024-02-03 09:05:42 +0530

    fixed aboutus

--- src/assets/pages/home/home.jsx
+++ src/assets/pages/home/home.jsx
@@ -1,10 +1,22 @@
 import styles from "./home.module.css";
-
+import { Link } from "react-router-dom";
 const Home = () => {
   return (
   <div className={styles.home}>
     <div className={styles.background}>
-      <img className={styles.cover} src="/home.png" />
+      <div className={styles.text}>
+      <h1 className={styles.heading}>
+        Care4edu 
+      </h1>
+      <h6 className={styles.para}>
+        We help students and professionals land their dream job
+
+      </h6>
+      <Link to="/apply">
+      <div className={styles.start}>Start</div>
+      </Link>
+      </div>
+
     </div>
   </div>
   );
